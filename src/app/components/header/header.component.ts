00a90cfdbca4420b8820712eb027e5893b52a1c4
van <1519999+uberstumbler@users.noreply.github.com>
2024-04-13 21:35:36 +0200

    feat: header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,32 @@
+import { NgClass } from '@angular/common';
+import { Component, OnDestroy, OnInit, WritableSignal, inject, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterModule, NgClass],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  private readonly router: Router = inject(Router);
+  activeMenu: WritableSignal<string> = signal('');
+
+  private routerEventSubscrition!: Subscription;
+
+  ngOnInit(): void {
+    this.routerEventSubscrition = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log('hhhh');
+        this.activeMenu.set(this.router.url.split('/')[1]);
+      }
+      console.log('🚀 ~ HeaderComponent ~ this.routerEventSubscrition=this.router.events.subscribe ~ this.router.url:', this.router.url);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routerEventSubscrition && this.routerEventSubscrition.unsubscribe();
+  }
+}
