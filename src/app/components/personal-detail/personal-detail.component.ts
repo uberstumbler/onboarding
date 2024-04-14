593eaef8ffe12d3f8626a0915485d3389407617b
van <1519999+uberstumbler@users.noreply.github.com>
2024-04-14 11:36:55 +0200

    feat: personal detail form
    missing validation for date of birth and correct design for radio button

--- src/app/components/personal-detail/personal-detail.component.ts
+++ src/app/components/personal-detail/personal-detail.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-detail',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './personal-detail.component.html',
-  styleUrl: './personal-detail.component.scss'
+  styleUrl: './personal-detail.component.scss',
 })
-export class PersonalDetailComponent {
+export class PersonalDetailComponent implements OnInit {
+  private readonly fb: FormBuilder = inject(FormBuilder);
 
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      gender: ['', [Validators.required]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      dateOfBirth: this.fb.group({
+        day: ['', [Validators.required]],
+        month: ['', [Validators.required]],
+        year: ['', [Validators.required]],
+      }),
+      nationality: ['', [Validators.required]],
+    });
+  }
+
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+  }
 }
